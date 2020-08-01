739db4a16c45dfb9195336b430b77664392e8c9c
yhjnk <35931032+yhjnk@users.noreply.github.com>
2020-08-02 02:58:11 +0800

    常用的导入库

---
+++ import_file.py
@@ -0,0 +1,15 @@
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import qgrid
+import numpy as np
+from sympy import *
+from scipy import integrate
+from ipywidgets import interact
+import ipywidgets as widgets
+import requests
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
